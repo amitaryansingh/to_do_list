13caa4ff052e61159068745f2e310f325698f4b0
Amit Aryan <aryanamit122@gmail.com>
2023-09-21 22:24:49 +0530

    My New Project
    My New project using html,css,js

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+    const signupForm = document.getElementById("signup-form");
+
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const username = document.getElementById("login-username").value;
+        const password = document.getElementById("login-password").value;
+        // Implement authentication logic here (e.g., validate against a database)
+        console.log(`Login: Username - ${username}, Password - ${password}`);
+    });
+
+    signupForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const username = document.getElementById("signup-username").value;
+        const password = document.getElementById("signup-password").value;
+        // Implement user registration logic here (e.g., store in a database)
+        console.log(`Sign Up: Username - ${username}, Password - ${password}`);
+    });
+});
